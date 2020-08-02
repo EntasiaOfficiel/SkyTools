43174d753e079da69b639af2896a44d7b9f024c4
iTrooz_ <team.mortelgames@gmail.com>
2020-08-02 21:10:42 +0200

    fix

--- src/fr/entasia/skytools/commands/CosmCmd.java
+++ src/fr/entasia/skytools/commands/CosmCmd.java
@@ -1,5 +1,6 @@
 package fr.entasia.skytools.commands;
 
+import fr.entasia.cosmetiques.utils.CosmAPI;
 import fr.entasia.cosmetiques.utils.InvsManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,7 +12,7 @@ public class CosmCmd implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         if(sender instanceof Player){
-            InvsManager.cosmMenuOpen((Player) sender);
+            CosmAPI.openCosmMenu((Player) sender);
         }
         return false;
     }
